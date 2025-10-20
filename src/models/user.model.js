5a5dbfe4ea9c74d15e34926b1720ae3514f81e77
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-10-20 11:50:22 -0300

    feat: create user model with email confirmation fields

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,2 +1,41 @@
+import { EntitySchema } from "typeorm";
 
-class User { }
+export default new EntitySchema({
+  name: "User",
+  columns: {
+    id: {
+      primary: true,
+      type: "uuid",
+      generated: "uuid",
+    },
+    email: {
+      type: "varchar",
+      unique: true,
+      nullable: false,
+    },
+    password: {
+      type: "varchar",
+      nullable: false,
+    },
+    isEmailConfirmed: {
+      type: "boolean",
+      default: false,
+    },
+    emailConfirmationToken: {
+      type: "varchar",
+      nullable: true,
+    },
+    emailConfirmationExpires: {
+      type: "timestamp",
+      nullable: true,
+    },
+    createdAt: {
+      type: "timestamp",
+      createDate: true,
+    },
+    updatedAt: {
+      type: "timestamp",
+      updateDate: true,
+    },
+  },
+});
