91ef59bfdb535f8c6f683c4629da2880e9f4dc5b
gabrieldiem <gabriel.diem5@gmail.com>
2025-10-21 02:56:40 -0300

    feat: added maps key

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
 import placesRoutes from "./places.routes.js";
+import mapsRoutes from "./maps.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/places", placesRoutes);
+router.use("/maps", mapsRoutes);
 
 const apiRouter = Router();
 apiRouter.use("/api", router);
