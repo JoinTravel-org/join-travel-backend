66935672ed9be7c3d11f2b6ab8d0d3b646c23e8e
gabrieldiem <gabriel.diem5@gmail.com>
2025-10-28 11:55:40 -0300

    try-fix: cors in local video playback

--- src/controllers/media.controller.js
+++ src/controllers/media.controller.js
@@ -28,6 +28,8 @@ export const getMediaFile = async (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
 
     logger.info(
       `Serving media file: ${id}, size: ${mediaRecord.fileSize}, type: ${mediaRecord.mimeType}`
